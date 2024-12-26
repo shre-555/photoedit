dcd318d26dfa0db98b25c1738980eef81d1af923
shre-555 <shreyakundu6782@gmail.com>
2024-12-26 19:13:42 +0530

    navbar styling

--- src/components/ImageEditor.jsx
+++ src/components/ImageEditor.jsx
@@ -3,9 +3,24 @@ import Dropzone from "./Dropzone"
 import './ImageEdit.css'
 function NavigationBar(){
     return(
+        <>
         <nav>
-        <img src="Logo.svg" alt="edit4Me" height="70"/>
-    </nav>
+            <img src="Logo.svg" alt="edit4Me" height="70" className='logo'/>
+            <div className='options'>
+                <button className='nav2'>
+                    <img src="Undo.png" alt="undo" height="25"/>
+                </button>
+                <button className='nav2'>
+                    <img src="Undo-1.png" alt="redo" />
+                </button>
+                <button className='nav2'>
+                    <img src="Upscale Sparkle.png" alt="undo" />
+                </button>
+                
+            </div>
+        </nav>
+        </>
+        
     )
 }
 
@@ -14,6 +29,7 @@ function ImageEditor(){
     return(
     <>
     <NavigationBar />
+    <br/>
     <Dropzone />
     </>
     );
